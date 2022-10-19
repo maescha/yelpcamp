ea5e0509016fe94d314231298b2a60723bca83b0
Josephine <sepiisy@gmail.com>
2022-10-19 16:35:30 -0600

    test push

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+    console.log("Database connected"); test
 });
 
 const app = express();
