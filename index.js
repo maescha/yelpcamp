90e5fa031a9028cd28db8ed05c5dc73d2348608c
maescha <sepiisy@gmail.com>
2022-11-16 17:12:46 -0700

    validate reviews in the backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const {campgroundSchema} = require('./schemas.js');
+const {campgroundSchema, reviewSchema} = require('./schemas.js');
 
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
@@ -45,6 +45,18 @@ const validateCampground = (req, res, next) => {
     } else {
         next();
     }
+}
+
+//validates reviewSchema
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
+
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+        } else {
+            next();
+        }
 }
 
 app.get('/', (req, res) => {
@@ -108,7 +120,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 }));
 
 //getting the reviews to submit and show on page
-app.post('/campgrounds/:id/reviews', catchAsync(async(req, res) => {
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
