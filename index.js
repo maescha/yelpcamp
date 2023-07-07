6d08c0d20f510fec0370bbf07653bc16e62404cd
maescha <sepiisy@gmail.com>
2023-07-07 17:23:58 -0600

    configuring passport

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+const User = require('./models/user');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useUnifiedTopology: true,
@@ -54,11 +56,27 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
+
+app.get('/fake', async (req, res) => {
+    const user = new User({email: 'asdasdasd@email.com', username:'dasdasade'});
+    const newUser = await User.register(user, 'pw');
+
+    res.send(newUser);
+
+});
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
