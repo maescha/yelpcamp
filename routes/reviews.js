39141ef24df4c5717d5249a3f67e5e3d5f4fe5a5
maescha <sepiisy@gmail.com>
2024-06-14 17:14:19 -0600

    associate review with author

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,14 +4,14 @@ const router = express.Router({ mergeParams: true });
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
-const { reviewSchema } = require('../schemas.js');
-const { validateReview } = require('../middleware.js')
+const { validateReview, isLoggedIn } = require('../middleware.js')
 
 const catchAsync = require('../utils/catchAsync');
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
