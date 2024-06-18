3258a1655e3e40d99e8a6016112fb31a11ef6abd
js <sepiisy@gmail.com>
2024-06-18 15:29:34 -0600

    refractor routes/campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,25 +7,25 @@ const campgrounds = require('../controllers/campgrounds.js');
 
 const router = express.Router();
 
-//renders all randomly generated campgrounds onto the 'main' page
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    //renders all randomly generated campgrounds onto the 'main' page
+    .get(catchAsync(campgrounds.index))
+    //setting up the endpoint where created data will be send to
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 //creating new campgrounds
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-//setting up the endpoint where created data will be send to
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.route('/:id')
+    //details page for selected campground, will also display reveiws
+    .get(catchAsync(campgrounds.showCampground))
+    //handles sending the inputted data to server to update database
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    //delete campground
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-//details page for selected campground, will also display reveiws
-router.get('/:id', catchAsync(campgrounds.showCampground));
 
 //edit campground details page
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.editForm));
-
-//handles sending the inputted data to server to update database
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-//delete campground
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
