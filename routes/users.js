b8b56e26515a8d03d6ca42568b0d7bfc85441409
js <sepiisy@gmail.com>
2024-06-18 15:33:46 -0600

    refractor routes/users

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,15 @@ const User = require('../models/user');
 const { storeReturnTo } = require('../middleware');
 const users = require('../controllers/users');
 
-//REGISTER ROUTE
-router.get('/register', users.renderRegister);
-
-router.post('/register', catchAsync(users.register));
+router.route('/register')
+    //REGISTER ROUTE
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
 //LOGIN ROUTE
-router.get('/login', users.renderLogin)
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 //LOGOUT ROUTES
 router.get('/logout', users.logout);
