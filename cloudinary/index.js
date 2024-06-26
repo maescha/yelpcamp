e25163091d3940a05cd37bc42c41edbaecf7b1fb
js <sepiisy@gmail.com>
2024-06-25 19:42:38 -0600

    FIXED TYPO THAT PREVENTED UPLOAD TO CLOUDINARY IN CLOUNDINARY/INDEX.JS, fixed routing

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -3,12 +3,12 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUNDINARY_KEY,
+    api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary,
+    cloudinary: cloudinary,
     folder: 'YelpCamp',
     allowedFormats: ['jpg', 'png', 'jpeg']
   });
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config()
+    require('dotenv').config();
 }
-
-console.log(process.env.SECRET);
-console.log(process.env.API_KEY);
 
 const express = require('express');
 const path = require('path');
