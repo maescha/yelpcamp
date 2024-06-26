52962900c1224c277eb5e033e9c216ca0f733659
js <sepiisy@gmail.com>
2024-06-25 19:05:36 -0600

    changes made for issues/7

--- middleware.js
+++ middleware.js
@@ -12,6 +12,7 @@ module.exports.storeReturnTo = (req, res, next) => {
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'you must be signed in to add new campgrounds!');
         return res.redirect('/login');
     }
